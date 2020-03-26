4aaca56bcf399f6de0dfef212dfc0fba46dc49f2
dmv5259 <dmv5259@psu.edu>
2020-03-26 17:47:57 -0400

    receipt rough draft..

---
+++ Receipt.py
@@ -0,0 +1,41 @@
+#Project: Brewing
+#Project Details: Brew Receipt
+#Course: IST 440W
+#Author: Deja Vasquez
+#Date Developed: 03/24/2020
+#Last Date Changed: 03/26/2020
+
+print("=======================")
+print(" Balrog Brewing ")
+print("=======================")
+
+finished=False
+
+taxRate=0.10
+itemNum=0
+subtotal=0.0
+tax=0.0
+total=0.0
+
+while(not finished):
+    prompt = "Enter price for Item #" + str(itemNum + 1) + ": $"
+    price = float(input(prompt))
+    if (price == 0):
+        finished = True
+    else:
+        subtotal += price
+        itemNum += 1
+        print("\tPrice entered: ${:.2f}".format(price))
+
+tax = subtotal * taxRate
+total = subtotal + tax
+
+print("=======================")
+print("Total Items Purchased: {:d}".format(itemNum))
+print("Subtotal: ${:.2f}".format(tax))
+print("Total: ${:.2f}".format(total))
+print("=======================")
+print(" Balrog Brewing Receipt")
+print("=======================")
+print("Thank you for your business!")
+
