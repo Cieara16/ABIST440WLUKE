e22a20216dcda19eb7d5c3accfe9d2dfbe424488
Brad <azul132@ymail.com>
2020-03-29 20:45:31 -0400

    Just getting ideas down. Absolutely will change.

--- Boiling.py
+++ Boiling.py
@@ -1,4 +1,6 @@
 #Begin Brew class
+import time
+
 class Brew():
     temperature = 0
     boilTime = 0
@@ -12,12 +14,15 @@ class Brew():
         self.minTemp = minTemp
         self.temperature = temperature
 
+    # We could just do the conversion once within the output statement rather that having a function. But I'm a dumbass so lemme know..
     def convertToFahrenheit(self):
         result = float((9 * self.temperature) / 5 + 32)
         return result
     def convertToCelcius(self):
         result = float((self.temperature - 32) * 5 / 9)
         return result
+
+
 
 def boil(self):
         #method for maintaining temperature
@@ -27,10 +32,25 @@ def maintainTemp(self):
     #method for maintaining temperature
     #for i in range[minTemperature]:
     #temperature -= temperature
-        
 
+    maxTemp = 110
+    minTemp = 100
+
+    #assuming temp is recorded as celsius would this work with the pi? Could we set this in the while loop timer below having it constantly record and check temp?
+    if self.temperature < minTemp:
+        self.temperature += 10
+        print('Too cold, increasing heat...')
+        return self
+    elif self.temperature > maxTemp:
+        self.temperature -= 10
+        print('Too hot, reducing heat...')
+        return self.temperature
+    else:
+        return self.temperature
+
+"""
     def maxTemperature(self, maxTemp):
-        maxTemp = 350
+        maxTemp = 110
         #if boil.temperature >= maxTemp:
         #print('Too hot, reducing heat...')
         #maintainTemp()
@@ -38,8 +58,18 @@ def maintainTemp(self):
     def minTemperature(self, minTemp):
         minTemp = 100
         return minTemp
+"""
 
 def main(self):
     Brew()
-    #work in progress
-    #new
+
+Sec = 0
+timeLoop = True
+
+# Begin Process. Not sure if we should include a 60min timer for boiling or leave that for the Brew master in between steps
+while timeLoop:
+    Sec += 1
+    time.sleep(1)
+    if Sec == 3600:
+        #I suppose code to execute and control temp with boiling will go here?
+        exit()
