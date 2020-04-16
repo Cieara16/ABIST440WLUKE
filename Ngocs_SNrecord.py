1f1038e0efbb3d36dbc87aa3f4497f79303c4838
Ngoc7-psu <59721719+Ngoc7-psu@users.noreply.github.com>
2020-04-15 20:29:02 -0400

    Add files via upload
    SN Record (Ngoc)

---
+++ Ngocs_SNrecord.py
@@ -0,0 +1,25 @@
+#Need to install requests package for python
+#easy_install requests
+import requests
+
+# Set the request parameters
+url = 'https://emplkasperpsu1.service-now.com/api/now/table/x_snc_beer_brewing_recipe'
+
+# Eg. User name="admin", Password="admin" for this code sample.
+user = 'ndt7'
+pwd = 'Tui@920Tyson!'
+
+# Set proper headers
+headers = {"Content-Type":"application/json","Accept":"application/json"}
+
+# Do the HTTP request
+response = requests.post(url, auth=(user, pwd), headers=headers ,data="{\"beer_name\":\"Ngoc's house special\"}")
+
+# Check for HTTP codes other than 200
+if response.status_code != 200:
+    print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:',response.json())
+    exit()
+
+# Decode the JSON response into a dictionary and use the data
+data = response.json()
+print(data)
