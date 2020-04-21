338ee284f0f462dcd52758dc85e1b00e5a028b94
Amrish Patel <36113881+amrish1991@users.noreply.github.com>
2020-04-21 01:23:34 -0400

    Update PrepTeam.py

--- PrepTeam.py
+++ PrepTeam.py
@@ -434,6 +434,43 @@ if __name__ == "__main__":
     data = response.json()
     print(data)
 
+    
+    global grain, grain_weight
+
+def get_ingredients():
+        #Need to install requests package for python
+        #easy_install requests
+
+        # Set the request parameters
+        url = 'https://emplkasperpsu1.service-now.com/api/now/table/x_snc_beer_brewing_ingredients?sysparm_query=Active%20%3D%20true&sysparm_fields=grain_weight%2Cgrains&sysparm_limit=1'
+
+        # Eg. User name="admin", Password="admin" for this code sample.
+        user = 'Amp6826'
+        pwd = '*****'
+
+        # Set proper headers
+        headers = {"Content-Type":"application/json","Accept":"application/json"}
+
+        # Do the HTTP request
+        response = requests.get(url, auth=(user, pwd), headers=headers )
+
+        # Check for HTTP codes other than 200
+        if response.status_code != 200:
+            print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:',response.json())
+            exit()
+
+        # Decode the JSON response into a dictionary and use the data
+        grain = response.json()['result'][0]['grain']
+        grain_weight = response.json()['result'][0]['grain_weight']
+        print('Grain: ' + grain)
+        print('Grain weight: ' + grain_weight)
+        return grain, grain_weight
+get()
+    
+    
+    
+    
+    
 #gather the grains
 btn = 1
 if btn == 1:
