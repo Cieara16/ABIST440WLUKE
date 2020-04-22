fd9f81871a200128457e34fccc383de5ed422406
Justin Hill <jmh7380@psu.edu>
2020-04-21 22:05:22 -0400

    Added more code 4/21

--- Boiling.py
+++ Boiling.py
@@ -7,6 +7,7 @@ import Adafruit_CharLCD as LCD
 import RPi.GPIO as GPIO
 import requests
 from datetime import datetime, date
+from Adafruit_LED_Backpack import SevenSegment
 
 # dh11 sensor
 sensor = 11
@@ -16,9 +17,8 @@ lcd_columns = 16
 lcd_rows = 2
 # Define Buzzer pin
 buzzerPin = 18
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(buzzerPin, GPIO.OUT)
+# Define Segment Address
+segment = SevenSegment.SevenSegment(address=0x70)
 
 global temperature, humidity, start, end, duration
 
@@ -34,9 +34,6 @@ temperature = temperature * 9 / 5.0 + 32
 # added the line to get closer to designated temperature rather than room temperature
 temperature *= 2
 
-now = datetime.now()
-currentTime = now.strftime("%H:%M:%S")
-
 
 # identifying sensor and pin and returning the temp and humidity
 def readTemperature(sensor, pin):
@@ -50,9 +47,9 @@ def __init__(self, temperature, humidity):
     self.temperature = temperature
 
 
-# retrieving most recent record from Mother Brew table
+# retrieving most recent record from Mother Brew table and all necessary items from that record to run the program
 def GetFromMotherbrew():
-    url = 'https://emplkasperpsu1.service-now.com/api/now/table/x_snc_beer_brewing_mother_brewv2?sysparm_query=numberISNOTEMPTY%5Eactive%3Dtrue%5Ebrew_phase%3DBoil&sysparm_limit=1'
+    url = 'https://emplkasperpsu1.service-now.com/api/now/table/x_snc_beer_brewing_mother_brewv2?sysparm_query=active%3Dtrue%5EnumberISNOTEMPTY%5EORDERBYDESCsys_created_on&sysparm_limit=1'
     user = 'kasper440'
     pwd = 'kasper440'
     # Servicenow Headers
@@ -64,8 +61,8 @@ def GetFromMotherbrew():
         print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:', response.json())
         exit()
 
-    # Decode the JSON response into a dictionary and use the data
-    global beerName, beerType, hops1, hops2, hops3, number, hops1Time, hops2Time, hops3Time, waterWeight
+    # Decode the JSON response into a dictionary and use the data as local variables
+    global beerName, beerType, hops1, hops2, hops3, number, hops1Time, hops2Time, hops3Time, waterWeight, sysId
     beerName = response.json()['result'][0]['beer_name']
     beerType = response.json()['result'][0]['beer_type']
     hops1 = response.json()['result'][0]['boil_hops1']
@@ -76,35 +73,34 @@ def GetFromMotherbrew():
     hops2Time = response.json()['result'][0]['boil_hops_2_time']
     hops3Time = response.json()['result'][0]['boil_hops_3_time']
     waterWeight = response.json()['result'][0]['water_by_weight']
+    sysId = response.json()['result'][0]['sys_id']
     print('Ticket Number: ' + number)
     print('Beer Name: ' + beerName)
     print('Beer Type: ' + beerType)
     print('1st Hops: ' + hops1)
     print('2nd Hops: ' + hops2)
     print('3rd Hops: ' + hops3)
+    print('Record ID: ' + sysId)
     time.sleep(1)
     # return the local variables
-    return beerName, beerType, hops1, hops2, hops3, number, hops1Time, hops2Time, hops3Time, waterWeight
+    return beerName, beerType, hops1, hops2, hops3, number, hops1Time, hops2Time, hops3Time, waterWeight, sysId
 
 
-# retrieving BoilPi tasks from the LKBrewTasks table
+# Function for retrieving BoilPi tasks from the LKBrewTasks table
 def GetFromBrewTasks():
     url = 'https://emplkasperpsu1.service-now.com/api/now/table/x_snc_beer_brewing_lkbrewtask?sysparm_query=active%3Dtrue%5Erpi_to_executeSTARTSWITHBoilPi%5Estate%3D-5&sysparm_limit=10'
     user = 'kasper440'
     pwd = 'kasper440'
-
     # Set proper headers
     headers = {"Content-Type": "application/json", "Accept": "application/json"}
-
     # Do the HTTP request
     response = requests.get(url, auth=(user, pwd), headers=headers)
-
     # Check for HTTP codes other than 200
     if response.status_code != 200:
         print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:', response.json())
         exit()
 
-    # Decode the JSON response into a dictionary and use the data
+    # Decode the JSON response into a dictionary and use the data as local variables
     global boilWaterTask1, addHopsTask2, drainWortTask3, checkTempTask4
     boilWaterTask1 = response.json()['result'][0]['number']
     shortDescr1 = response.json()['result'][0]['short_description']
@@ -124,34 +120,28 @@ def GetFromBrewTasks():
     return boilWaterTask1, shortDescr1, addHopsTask2, shortDescr2, drainWortTask3, shortDescr3, checkTempTask4, shortDescr4
 
 
+# Function for GET request from ingredients table
 def GetFromIngredients():
-    # Set the request parameters
     url = 'https://emplkasperpsu1.service-now.com/api/now/table/x_snc_beer_brewing_ingredients?sysparm_limit=1'
-
-    # Eg. User name="admin", Password="admin" for this code sample.
     user = 'kasper440'
     pwd = 'kasper440'
-
     # Set proper headers
     headers = {"Content-Type": "application/json", "Accept": "application/json"}
-
     # Do the HTTP request
     response = requests.get(url, auth=(user, pwd), headers=headers)
-
     # Check for HTTP codes other than 200
     if response.status_code != 200:
         print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:', response.json())
         exit()
-
-    # Decode the JSON response into a dictionary and use the data
+    # Decode the JSON response into a dictionary and use the data as local variables
     data = response.json()
     hops = response.json()['result'][0]['hops']
     # print('Hops: ' + hops)
     # print(data)
-
     return hops
 
 
+# Function for posting data to Servicenow. Can be changed..
 def Post():
     # Set the request parameters
     url = 'https://emplkasperpsu1.service-now.com/api/now/table/x_snc_beer_brewing_boil'
@@ -173,6 +163,7 @@ def Post():
     return data
 
 
+# Posting the final temperature after cooling the wort to Servicenow
 def PostFinalTemp():
     # Set the request parameters
     url = 'https://emplkasperpsu1.service-now.com/api/now/table/x_snc_beer_brewing_boil'
@@ -193,20 +184,53 @@ def PostFinalTemp():
     return data
 
 
+# used to check system functionality before continuing
+# if all is good, then pass
+# if there are problems, then throw error
 def systemCheck():
-    # used to check system functionality before continuing
-    # if all is good, then pass
-    # if there are problems, then throw error
     pass
 
 
+# Function for adding water to the brew chamber
+# Amount of water requested by customer in Servicenow
 def AddWater():
     print('Amount of water: ' + waterWeight + 'gal.')
     print('Adding water to brew chamber...')
     time.sleep(15)
     print('Water added to brew chamber.')
+    time.sleep(1)
 
 
+# Function for onboard segment clock to display current time
+def Clock():
+    segment.begin()
+    # Continually update the time on a 4 char, 7-segment display
+    try:
+        while (True):
+            now = datetime.now()
+            hour = now.hour
+            minute = now.minute
+            second = now.second
+            segment.clear()
+            # Set hours
+            segment.set_digit(0, int(hour / 10))  # Tens
+            segment.set_digit(1, hour % 10)  # Ones
+            # Set minutes
+            segment.set_digit(2, int(minute / 10))  # Tens
+            segment.set_digit(3, minute % 10)  # Ones
+            # Toggle colon
+            segment.set_colon(second % 2)  # Toggle colon at 1Hz
+            # Write the display buffer to the hardware.  This must be called to
+            # update the actual display LEDs.
+            segment.write_display()
+            # Wait a quarter second (less than 1 second to prevent colon blinking getting$
+            time.sleep(0.25)
+    except KeyboardInterrupt:
+        segment.clear()
+        segment.write_display()
+
+
+# Reading temperature and humidity from the dh11 sensor and displaying it
 def tempDisplay():
     print()
     print('Beginning Task: ' + boilWaterTask1)
@@ -224,79 +248,109 @@ def tempDisplay():
                 count += 1
                 time.sleep(1)
                 print(count)
-                if (count == hops1Time):
+                if (count == int(hops1Time)):
                     print('Beginning Task: ' + addHopsTask2)
                     print('Adding ' + hops1 + ' hops.')
-                if (count == hops2Time):
+                if (count == int(hops2Time)):
                     print('Adding ' + hops2 + ' hops.')
-                if (count == hops3Time):
+                if (count == int(hops3Time)):
                     print('Adding ' + hops3 + ' hops.')
 
             else:
                 lcd.message('Failed to get reading. Try again!')
+
         except KeyboardInterrupt:
             # Turn the screen off
             lcd.clear()
             lcd.set_backlight(1)
 
     print(boilWaterTask1 + ' Boiling process complete')
+    print()
+    time.sleep(5)
     Post()
 
 
+# Function for cooling the wort so the fermenting team can take over
 def WortCooled():
     global temperature
-    print()
     print('Cooling Wort....')
     temperature -= 40
-    time.sleep(10)
+    time.sleep(20)
+    print('Wort has been cooled to ' + str(temperature) + ' degrees.')
     if (temperature >= 79):
         print('Wort still too hot... decreasing temperature')
         temperature -= 30
-        time.sleep(10)
+        time.sleep(20)
     print()
     print('Wort has been cooled to ' + str(temperature) + ' degrees.')
-
     print('Sending data to Servicenow...')
-    time.sleep(5)
+    time.sleep(10)
+    print('Data sent.')
     PostFinalTemp()
 
 
+# Function to transfer wort to heat exchanger
 def WortDrained():
     print()
-    print('Beginning Task: ' + drainWortTask3)
+    print('Beginning ' + drainWortTask3)
     print('Transferring wort to heat exchanger...')
     time.sleep(10)
     print('Wort has been transferred.')
-
-
-def QualityCheck():
-    GPIO.ouptut(buzzerPin, GPIO.LOW)
+    print(drainWortTask3 + ' Complete.')
+    print()
     time.sleep(1)
+
+
+# Function for quality check
+def QualityCheck():
     print('Beginning Quality Check')
+    print('Beginning ' + checkTempTask4)
+    print('Checking temperature...')
+    time.sleep(20)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(buzzerPin, GPIO.OUT)
+    # Make buzzer sound
+    GPIO.output(buzzerPin, GPIO.HIGH)
+    time.sleep(1)
+    # Stop buzzer sound
+    GPIO.output(buzzerPin, GPIO.LOW)
+    GPIO.cleanup()
+    print('Quality Check Complete')
 
 
+def ResetClean():
+    motor = ' '
+
+
+#     motor = Stepmotor()
+#     print('Beginning Quality Check')
+#     motor.turnSteps(1)
+#     print('One Step')
+#     time.sleep(1)
+#     motor.turnSteps(20)
+#     print('20 steps')
+#     print("quarter turn")
+#     motor.turnDegrees(90)
+#     print("moving stopped")
+#     motor.close()
+
+# Function for posting final data to the Log Table in Servicenow
 def PostToLogTable():
     # Set the request parameters
     url = 'https://emplkasperpsu1.service-now.com/api/now/table/x_snc_beer_brewing_log_table'
-
-    # Eg. User name="admin", Password="admin" for this code sample.
-    user = 'admin'
-    pwd = 'admin'
-
-    # Set proper headers
+    user = 'kasper440'
+    pwd = 'kasper440'
     headers = {"Content-Type": "application/json", "Accept": "application/json"}
-
     # Do the HTTP request
     response = requests.post(url, auth=(user, pwd), headers=headers,
                              data="{\"boiling_temperature\":\"" + str(temperature) + "\",\"boil_start_time\":\"" + str(
                                  start) + "\",\"boil_end_time\":\"" + str(end) + "\",\"boil_duration\":\"" + str(
-                                 duration) + "\",\"boil_quality_check\":\"Complete\",\"boil_reset_clean\":\"\",\"boil_errors\":\"\"}")
-
+                                 duration) + "\",\"boil_quality_check\":\"True\",\"boil_reset_clean\":\"\",\"boil_errors\":\"\",\"number\":\"" + str(
+                                 number) + "\"}")
     # Check for HTTP codes other than 200
     if response.status_code != 201:
         print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:', response.json())
         exit()
-
     # Decode the JSON response into a dictionary and use the data
     data = response.json()
     print(data)
@@ -304,9 +358,8 @@ def PostToLogTable():
 
 def main():
     global start, end, duration
-    #     processDuration = 0
-    #     processDuration += 1
     print('Beginning the Boiling Process')
+    # Clock()
     start = datetime.now()
     print(start)
     time.sleep(1)
@@ -318,9 +371,9 @@ def main():
     AddWater()
     tempDisplay()
     WortCooled()
-    #     Quality Check()
     WortDrained()
-    #     ResetClean()
+    QualityCheck()
+    # ResetClean()
     print("done")
     end = datetime.now()
     print('End time: ')
@@ -328,11 +381,12 @@ def main():
     duration = end - start
     print('Process Duration: ')
     print(duration)
-    return start, end, duration
     PostToLogTable()
-    import GetFromMotherBrew
+    lcd.clear()
+    segment.clear()
 
 
+# Run the program
 main()
-
+import GetFromMotherBrew
 
