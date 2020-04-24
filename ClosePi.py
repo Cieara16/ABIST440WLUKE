a9232c3bcb95a03665f3a93070b92240b8242399
Ryan Carey <ruc230@psu.edu>
2020-04-24 17:29:47 -0400

    finishing up close development.

--- ClosePi.py
+++ ClosePi.py
@@ -7,7 +7,7 @@ def print_label(beer_name, abv, keg_volume, current_date):
     text_for_label = 'text 190,490 "' + beer_name + ' ' + current_date + ' ABV: ' + abv + '% ' + keg_volume + ' Gal"'
     label_write = subprocess.Popen(['/usr/bin/convert', '-pointsize', '18', '-draw', text_for_label, 'beer_label.png', 'beer_label_withtext.png'])
     label_write.wait()
-    #subprocess.Popen(['/usr/bin/lp', '-d', 'HP_DeskJet_2130_series', '-o', 'orientation-requested=3', 'beer_label_withtext.png'])
+    subprocess.Popen(['/usr/bin/lp', '-d', 'HP_DeskJet_2130_series', '-o', 'orientation-requested=3', 'beer_label_withtext.png'])
 
 
 # Set the request parameters
