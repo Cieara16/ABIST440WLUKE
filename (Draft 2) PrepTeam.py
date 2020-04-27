0c3c21c4e5d93cd95f71300a5df53d180201ec75
Amrish Patel <36113881+amrish1991@users.noreply.github.com>
2020-04-27 14:56:55 -0400

    Update (Draft 2) PrepTeam.py

--- (Draft 2) PrepTeam.py
+++ (Draft 2) PrepTeam.py
@@ -241,6 +241,46 @@ def Get_Task_for_CrowPi():
     print(Task4)
     print(shortDescTask4)
 
+def Loop():
+    url = 'https://emplkasperpsu1.service-now.com/api/now/table/x_snc_beer_brewing_mother_brewv2?sysparm_query=sys_created_onRELATIVEGT%40minute%40ago%401&sysparm_limit=1'
+    user = 'kasper440'
+    pwd = 'kasper440'
+    # Set proper headers
+    headers = {"Content-Type": "application/json", "Accept": "application/json"}
+    # Do the HTTP request
+    response = requests.get(url, auth=(user, pwd), headers=headers)
+
+    # Check for HTTP codes other than 200
+    if response.status_code != 200:
+        print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:', response.json())
+        exit()
+
+    # Decode the JSON response into a dictionary and use the data
+    result = response.json()['result']
+    emptyList = []
+
+    while (result == emptyList):
+        url = 'https://emplkasperpsu1.service-now.com/api/now/table/x_snc_beer_brewing_mother_brewv2?sysparm_query=sys_created_onRELATIVEGT%40minute%40ago%401&sysparm_limit=1'
+        user = 'kasper440'
+        pwd = 'kasper440'
+        # Set proper headers
+        headers = {"Content-Type": "application/json", "Accept": "application/json"}
+        # Do the HTTP request
+        response = requests.get(url, auth=(user, pwd), headers=headers)
+
+        # Check for HTTP codes other than 200
+        if response.status_code != 200:
+            print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:', response.json())
+            exit()
+
+        # Decode the JSON response into a dictionary and use the data
+        result = response.json()['result']
+        emptyList = []
+        print(result)
+        time.sleep(25)
+    #if (result != emptyList):
+        #main()
+        
 
 # Stepper motor 1
 # test equipment
