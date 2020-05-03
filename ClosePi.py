d084392c518c1a2bc66746618d31b11aabfa1d4c
Ryan Carey <ruc230@psu.edu>
2020-05-03 18:43:44 -0400

    removed duplicate line

--- ClosePi.py
+++ ClosePi.py
@@ -86,10 +86,9 @@ def main():
         print(current_close_task['number'])
         if short_description.startswith('print') or description.startswith('print'):
             print_label(beer_name, abv, keg_volume, current_date)
-            patch_brew_task(task_id)
         elif short_description.startswith('attach') or description.startswith('attach'):
             enter = input("Press enter when the label has been attached...")
-            patch_brew_task(task_id)
+        patch_brew_task(task_id)
         end_time = datetime.datetime.now()
         patch_end_time(log_post_sys_id, end_time)
         print('Logging complete.')
